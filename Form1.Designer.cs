6d9db9c477b7582c776a74e8bd5021b43e4e91c7
Arkadiusz Kubiszewski <arek.kubiszewski@gmail.com>
2023-04-11 21:51:20 +0200

    Added button names

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,18 @@
             newButton = new Button();
             saveButton = new Button();
             deleteButton = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
             ((System.ComponentModel.ISupportInitialize)inventoryGridView).BeginInit();
             SuspendLayout();
             // 
@@ -61,9 +73,9 @@
             // newButton
             // 
             newButton.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
-            newButton.Location = new Point(12, 228);
+            newButton.Location = new Point(12, 242);
             newButton.Name = "newButton";
-            newButton.Size = new Size(343, 59);
+            newButton.Size = new Size(350, 45);
             newButton.TabIndex = 2;
             newButton.Text = "New";
             newButton.UseVisualStyleBackColor = true;
@@ -71,9 +83,9 @@
             // saveButton
             // 
             saveButton.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
-            saveButton.Location = new Point(462, 228);
+            saveButton.Location = new Point(466, 242);
             saveButton.Name = "saveButton";
-            saveButton.Size = new Size(343, 59);
+            saveButton.Size = new Size(350, 45);
             saveButton.TabIndex = 3;
             saveButton.Text = "Save";
             saveButton.UseVisualStyleBackColor = true;
@@ -81,12 +93,108 @@
             // deleteButton
             // 
             deleteButton.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
-            deleteButton.Location = new Point(910, 228);
+            deleteButton.Location = new Point(903, 242);
             deleteButton.Name = "deleteButton";
-            deleteButton.Size = new Size(343, 59);
+            deleteButton.Size = new Size(350, 45);
             deleteButton.TabIndex = 4;
             deleteButton.Text = "Delete";
             deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(350, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 187);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(350, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(466, 187);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(350, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(466, 139);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(350, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(903, 187);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(350, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(903, 139);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(350, 23);
+            textBox6.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(350, 23);
+            label2.TabIndex = 11;
+            label2.Text = "EAN:";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(466, 113);
+            label3.Name = "label3";
+            label3.Size = new Size(350, 23);
+            label3.TabIndex = 12;
+            label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(903, 113);
+            label4.Name = "label4";
+            label4.Size = new Size(350, 23);
+            label4.TabIndex = 13;
+            label4.Text = "Category:";
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(12, 161);
+            label5.Name = "label5";
+            label5.Size = new Size(350, 23);
+            label5.TabIndex = 14;
+            label5.Text = "Price:";
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(466, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(350, 23);
+            label6.TabIndex = 15;
+            label6.Text = "Description:";
+            // 
+            // label7
+            // 
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(903, 165);
+            label7.Name = "label7";
+            label7.Size = new Size(350, 23);
+            label7.TabIndex = 16;
+            label7.Text = "Quantity:";
             // 
             // InventoryTracker
             // 
@@ -94,6 +202,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DodgerBlue;
             ClientSize = new Size(1265, 744);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox5);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(deleteButton);
             Controls.Add(saveButton);
             Controls.Add(newButton);
@@ -104,6 +224,7 @@
             Load += InventoryTracker_Load;
             ((System.ComponentModel.ISupportInitialize)inventoryGridView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -113,5 +234,17 @@
         private Button newButton;
         private Button saveButton;
         private Button deleteButton;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
     }
 }
